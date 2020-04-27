5c92f1b13fa50938cbf54d8a64d3f431a3bc36fb
Bennett <Aleks.Bennett@bregroup.com>
2020-04-27 14:56:50 +0100

    Added exception handling for controllers

---
+++ src/main/java/guru/springframework/brewery/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,34 @@
+package guru.springframework.brewery.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/* Add default controller validation */
+@ControllerAdvice
+public class MvcExceptionHandler {
+    
+    /* handle validation errors and return to user */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e){
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+        
+        e.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List> handleBindException(BindException e){
+        return new ResponseEntity<List>(e.getAllErrors(), HttpStatus.BAD_REQUEST);
+    }
+}
