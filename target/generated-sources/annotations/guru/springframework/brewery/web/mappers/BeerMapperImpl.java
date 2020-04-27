8f7739d06d75995147a366120494715a0d99c99f
Bennett <Aleks.Bennett@bregroup.com>
2020-04-27 14:57:27 +0100

    Added mapstruct and implemented for  beer

---
+++ target/generated-sources/annotations/guru/springframework/brewery/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,13 @@
+package guru.springframework.brewery.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-04-27T14:51:21+0100",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (AdoptOpenJDK)"
+)
+@Component
+public class BeerMapperImpl extends BeerMapper {
+}
